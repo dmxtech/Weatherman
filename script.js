66128c7d07b0c31dc37721cad234f2bec61d5165
cjclap <dmxtechinn@gmail.com>
2022-08-08 22:05:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//Declare variables for API
+var lat = document.getElementById('latitude'); 
+var lon = document.getElementById('longitude');
+var lang = 'en';
+var units ='metric';
+var search = document.getElementById('search').value;
+var key= 'db1cb261132a669040597c8d3f2222b6';
+var url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+
+
+fetch(url)
+      .then((resp) => {
+        console.log(resp);
+        if (!resp.ok) throw new Error(resp.statusText);
+        return resp.json();
+      })
+      .then((data) => {
+        app.showWeather(data);
+      })
+
+
+    
